d1736713109f9b1a71d1231a4619978949f9424a
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-03-08 12:35:28 +0200

    -

--- المستوى الثالث/35 _ Check Number In Array/35 _ Check Number In Array/35 _ Check Number In Array.cpp
+++ المستوى الثالث/35 _ Check Number In Array/35 _ Check Number In Array/35 _ Check Number In Array.cpp
@@ -1,70 +1,68 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int ReadNumber()
-{ 
-	int Number; 
-	cout << "\nPlease enter a number to search for?\n"; 
-	cin >> Number; return Number;
-} 
+{
+	int Number;
+	cout << "\nPlease enter a number to search for?\n";
+	cin >> Number;
+	return Number;
+}
 
-int RandomNumber(int From, int To) {
-	//Function to generate a random number
+int RandomNumber(int From, int To)
+{
 	int randNum = rand() % (To - From + 1) + From;
 	return randNum;
 }
 
-void FillArrayWithRandomNumbers(int arr[100], int& arrLength) {
-	cout << "\nEnter number of elements:\n"; 
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+	cout << "\nEnter number of elements:\n";
 	cin >> arrLength;
-	
+
 	for (int i = 0; i < arrLength; i++)
 		arr[i] = RandomNumber(1, 100);
 }
 
 void PrintArray(int arr[100], int arrLength)
-{ 
-	for (int i = 0; i < arrLength; i++) 
+{
+	for (int i = 0; i < arrLength; i++)
 		cout << arr[i] << " ";
-	cout << "\n"; 
-} 
+	cout << "\n";
+}
 
-short FindNumberPositionInArray(int Number, int arr[100], int arrLength)
+short FindNumberPositionInArray(int arr[100], int arrLength, int Number)
 {
 	for (int i = 0; i < arrLength; i++)
 	{
 		if (arr[i] == Number)
 			return i;
-		// return the index  
-	} 
-	//if you reached here, this means the number is not found
-	return -1; 
-} 
-
-bool IsNumberInArray(int Number, int arr[100], int arrLength)
-{ 
-	return FindNumberPositionInArray(Number, arr, arrLength) != -1;
+	}	
+	return -1;
 }
 
-int main() {
-	//Seeds the random number generator in C++, called only once
-	srand((unsigned)time(NULL));
-	int arr[100], arrLength; 
+bool IsNumberInArray(int arr[100], int arrLength, int Number)
+{
+	return FindNumberPositionInArray(arr, arrLength, Number) != -1;
+}
 
-	FillArrayWithRandomNumbers(arr, arrLength); 
-
+int main() { 
+	//Seeds the random number generator in C++, called only once 
+	srand((unsigned)time(NULL)); 
+	
+	int arr[100], arrLength;
+	FillArrayWithRandomNumbers(arr, arrLength);
+	
 	cout << "\nArray 1 elements:\n"; 
 	PrintArray(arr, arrLength); 
 	
-	int Number = ReadNumber(); 
-	cout << "\nNumber you are looking for is: " << Number << endl; 
+	int Number = ReadNumber();
+	cout << "\nNumber you are looking for is: " << Number << endl;
 	
-	if (!IsNumberInArray(Number, arr, arrLength))   
-		cout << "No, The number is not found :-(\n";
-	else     
-		cout << "Yes it is found :-)\n"; 
-	
+	if (!IsNumberInArray(arr, arrLength, Number))
+		cout << "No, The number is not found :-(\n"; 
+	else        
+		cout << "Yes it is found :-)\n";     
 	return 0; 
-
-}
+} 
