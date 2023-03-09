38097a26a933d6698ab4b300c9d12306d04c9034
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-03-09 12:41:46 +0200

    CPP Level 3

---
+++ المستوى الثالث/37 _ Resolve Copy Array Using ADD ARRAY ELEMENT/37 _ Resolve problem 28_2 Copy Array Using ADD ARRAY ELEMENT/37 _ Resolve problem 28_2 Copy Array Using ADD ARRAY ELEMENT.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumber(1, 100);
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+void AddArrayElement(int arr[100], int& arrLength, int Number)
+{
+    arrLength++;
+    arr[arrLength - 1] = Number;
+}
+
+void CopyArrayUsingAddArrayElement(int arrSource[100], int arrDistination[100], int arrLength, int& arrDistinationLength)
+{
+    for (int i = 0; i < arrDistinationLength; i++)
+        AddArrayElement(arrDistination, arrDistinationLength, arrSource[i]);
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength = 0, arr2Length = 0;
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    int arr2[100];
+    CopyArrayUsingAddArrayElement(arr, arr2, arrLength, arr2Length);
+
+    cout << "\nArray 1 elements:\n";
+    PrintArray(arr, arrLength);
+
+    cout << "\nArray 2 elements after copy:\n";
+    PrintArray(arr2, arr2Length);
+
+    return 0;
+}
