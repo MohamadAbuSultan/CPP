9c4f3a20ea2cdb1ea783db51767239b94f792f17
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-03-18 16:22:11 +0200

    CPP Level 3

---
+++ المستوى الثالث/44 _ Count Positive Numbers in Array/44 _ Count Positive Numbers in Array/44 _ Count Positive Numbers in Array.cpp
@@ -0,0 +1,56 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int RandomNumbers(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+    cout << "\nEnter Number OF Elements\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumbers(-100, 100);
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int CountPositiveNumbers(int arr[100], int arrLength)
+{
+    int Counter = 0;
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i] >= 0)
+            Counter++;
+    }
+    return Counter;
+}
+
+int main()
+{
+    //Seeds the random number generator in C++, called only once 
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nPositive Numbers count is: ";
+    cout << CountPositiveNumbers(arr, arrLength) << endl;
+
+    return 0;
+}
