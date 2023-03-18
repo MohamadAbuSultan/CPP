020f623b3f315d53b7e851883d6ea0093acbbf1e
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-03-18 17:44:14 +0200

    CPP Level 3

---
+++ المستوى الثالث/48 _ Floor/48 _ Floor/48 _ Floor.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+float ReadNumber()
+{
+    float Number;
+    cout << "Enter A Number : ";
+    cin >> Number;
+    return Number;
+}
+
+int MyFloor(float Number)
+{
+    if (Number > 0)
+        return int(Number);
+    else
+        return int(Number) - 1;
+}
+
+int main()
+{
+    float Number = ReadNumber();
+    cout << "\nMy floor Result : " << MyFloor(Number) << endl;
+    cout << "C++ floor Result : " << floor(Number) << endl;
+}
