8abb17b20bf76a0d9327b6885853238bc3a2667b
MohamadKhaled <mohamadknsult2@gmail.com>
2024-05-08 15:48:18 +0300

    May _ 2024

---
+++ 05 - Algorithms & Problem-Solving Level 2/10 _ Print Digits in Order/10 _ Print Digits in Order/10 _ Print Digits in Order.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message){
+	int Number;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+
+int ReversedOrder(int Number){
+	int Reminder = 0, Result = 0;
+	while (Number > 0)
+	{
+		Reminder = Number % 10;
+		Number = Number / 10;
+		Result = Result * 10 + Reminder;
+	}
+	return Result;
+}
+
+void DigitInOrder(int Number) {
+	int Reminder = 0;
+	while (Number > 0)
+	{
+		Reminder = Number % 10;
+		Number = Number / 10;
+		cout << Reminder << endl;
+	}
+}
+
+int main()
+{
+	DigitInOrder(ReversedOrder(ReadPositiveNumber("M")));
+}
