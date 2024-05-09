7f32913b7114c1270a56a6a65fd57942b6bbee4a
MohamadKhaled <mohamadknsult2@gmail.com>
2024-05-09 18:38:13 +0300

    may _ 2024

---
+++ 05 - Algorithms & Problem-Solving Level 2/11 _ Plaindrome Numbers/11 _ Plaindrome Numbers/11 _ Plaindrome Numbers.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message) {
+	int Number;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+
+int ReversedNumber(int Number) {
+	int Reminder = 0, Result = 0;
+	while (Number > 0)
+	{
+		Reminder = Number % 10;
+		Number = Number / 10;
+		Result = Result * 10 + Reminder;
+	}
+	return Result;
+}
+
+bool IsPlaindromNumber(int Number) {
+	return Number == ReversedNumber(Number);
+}
+
+void PrintResult(int Number) {
+	if (IsPlaindromNumber(Number)) cout << Number << " is Plaindrom Number";
+	else cout << Number << " is not plaindrom number";
+}
+int main()
+{
+	PrintResult(ReadPositiveNumber("Enter A Positive Number : "));
+}
