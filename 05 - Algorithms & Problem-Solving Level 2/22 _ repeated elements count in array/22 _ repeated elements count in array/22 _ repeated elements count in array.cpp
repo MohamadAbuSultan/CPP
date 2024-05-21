76db4e2bdcf545e220f34a28d0122ecf11e600e9
MohamadKhaled <mohamadknsult2@gmail.com>
2024-05-21 16:49:50 +0300

    May-2024

--- 05 - Algorithms & Problem-Solving Level 2/22 _ repeated elements count in array/22 _ repeated elements count in array/22 _ repeated elements count in array.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/22 _ repeated elements count in array/22 _ repeated elements count in array/22 _ repeated elements count in array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -12,11 +13,42 @@ int ReadPositiveNumber(string Message) {
 	return Number;
 }
 
-int ReadArrayNumbers() {
+vector<int> ReadArrayNumbers(int Number) {
+	vector<int> result;
+	int Number2;
+	for (int i = 1; i <= Number; i++)
+	{
+		cout << "Element [" << i << "]: ";
+		cin >> Number2;
+		result.push_back(Number2);
+	}
+	return result;
+}
 
+int PrintRepeatedElementsCount(vector<int> &Result, short numberToCheck) {
+	int Counter = 0;
+	for (int i = 0; i < Result.size(); i++)
+	{
+		if (Result[i] == numberToCheck) Counter++;
+	}
+	return Counter;
+}
+
+void PrintOriginalArray(vector<int>& Result) {
+	for (int i = 0; i < Result.size(); i++)
+	{
+		cout << Result[i] << " ";
+	}
 }
 
 int main()
 {
+	int Number1 = ReadPositiveNumber("Enter A Positive Integer");
+	vector<int> NumbersOfArray = ReadArrayNumbers(Number1);
 
+	int Number2 = ReadPositiveNumber("Enter A Number To Check");
+	cout << "Count of repeated elements: " << PrintRepeatedElementsCount(NumbersOfArray, Number2) << endl;
+
+	cout << "Original array: ";
+	PrintOriginalArray(NumbersOfArray);
 }
