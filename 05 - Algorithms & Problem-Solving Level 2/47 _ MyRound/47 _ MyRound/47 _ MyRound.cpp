033042eabb48092aff02755df1f3ab37e32953d3
MohamadKhaled <mohamadknsult2@gmail.com>
2024-05-27 21:53:09 +0300

    May-2024

--- 05 - Algorithms & Problem-Solving Level 2/47 _ MyRound/47 _ MyRound/47 _ MyRound.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/47 _ MyRound/47 _ MyRound/47 _ MyRound.cpp
@@ -9,17 +9,28 @@ float ReadNumber(string message) {
 	return number;
 }
 
-float MyRound(float& number) {
-	float reminder = ((int)number % 10) * 10;
-	
-	if (reminder >= 5)
-		return number + 1;
-	return number;
+float GetFractionPart(float number) {
+	return number - (int)number;
+}
+
+float MyRound(float number) {
+	int intPart = (int)number;
+
+	float fractionPart = GetFractionPart(number);
+
+	if (abs(fractionPart) >= 0.5)
+		if (number > 0)
+			return ++intPart;
+		else
+			return --intPart;
+	else 
+		return intPart;
 }
 
 int main()
 {
 	float number = ReadNumber("Enter Number");
-
-	cout << MyRound(number);
+	
+	cout << "Round Function: " << round(number) << endl;
+	cout << "My Round Function: " << MyRound(number);
 }
