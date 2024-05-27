1924a140df2bb107f44d0b0c01eb1d8aafd0e2b4
MohamadKhaled <mohamadknsult2@gmail.com>
2024-05-27 17:48:16 +0300

    May-2024

---
+++ 05 - Algorithms & Problem-Solving Level 2/43 _ Copy Even Numbers In Array/43 _ Copy Even Numbers In Array/43 _ Copy Even Numbers In Array.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int ReadPositiveNumber(string message) {
+	int number;
+
+	do
+	{
+		cout << message << " ";
+		cin >> number;
+	} while (number <= 0);
+
+	return number;
+}
+
+int GenerateRandomNumber(int from, int to) {
+	return rand() % (to - from + 1) + from;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int arrLength) {
+	for (int i = 0; i < arrLength; i++)
+		arr[i] = GenerateRandomNumber(1, 100);
+}
+
+void PrintArray(int arr[100], int arrLength) {
+	for (int i = 0; i < arrLength; i++)
+		cout << arr[i] << " ";
+}
+
+int CountEvenNumbers(int arr[100], int arrLength) {
+	int counter = 0;
+
+	for (int i = 0; i < arrLength; i++)
+		if (arr[i] % 2 == 0) 
+			counter++;
+
+	return counter;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100];
+	int arrLength = ReadPositiveNumber("Enter A Positive Number: ");
+
+	FillArrayWithRandomNumbers(arr, arrLength);
+
+	cout << "\nArray1 Elements: ";
+	PrintArray(arr, arrLength);
+
+	cout << "\n\nCount Even: ";
+	cout << CountEvenNumbers(arr, arrLength);
+
+	cout << endl;
+
+	return 0;
+}
