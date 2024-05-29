4a36708549c148bdf2251272b5da39a2611e596a
MohamadKhaled <mohamadknsult2@gmail.com>
2024-05-29 11:26:31 +0300

    Stone-Paper-Scissor Project

---
+++ Course5- Project1 Stone-Paper-Scissor/Course5- Project1 Stone-Paper-Scissor/Course5- Project1 Stone-Paper-Scissor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct Round {
+    string playerChoice;   
+    string computerChoice; 
+    string winner;         
+};
+
+struct GameResults {
+    int numRounds;         
+    int playerWins;        
+    int computerWins;      
+    int draws;              
+    string winner;    
+};
+
+int main()
+{
+
+}
