d9663e3d87b9cf90210e805fefd408b329de0171
MohamadKhaled <mohamadknsult2@gmail.com>
2024-05-29 15:38:14 +0300

    May-2024

--- Course5- Project1 Stone-Paper-Scissor/Course5- Project1 Stone-Paper-Scissor/Course5- Project1 Stone-Paper-Scissor.cpp
+++ Course5- Project1 Stone-Paper-Scissor/Course5- Project1 Stone-Paper-Scissor/Course5- Project1 Stone-Paper-Scissor.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int ReadPositiveNumber(string message) {
+    int number;
+    do
+    {
+        cout << message << " ";
+        cin >> number;
+    } while (number <= 0);
+    return number;
+}
+
+int GenerateRandomNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
+}
+
 struct Round {
     string playerChoice;   
     string computerChoice; 
@@ -15,6 +29,18 @@ struct GameResults {
     int draws;              
     string winner;    
 };
+
+enum enChoice { Paper = 1, stone = 2, scissors = 3 };
+
+enChoice UserChoice() {
+    int userChoice = ReadPositiveNumber("\nEnter Your Choice : ");
+    return (enChoice)userChoice;
+}
+
+enChoice ComputerChoice() {
+    int computerChoice = GenerateRandomNumber(1,3);
+    return (enChoice)computerChoice;
+}
 
 int main()
 {
