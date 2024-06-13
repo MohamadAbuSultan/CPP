4024ecad20e05c07ff59e85b5d33cfed1b408b14
MohamadKhaled <mohamadknsult2@gmail.com>
2024-06-14 00:53:29 +0300

    June-2024

--- 07 - Algorithms & Problem Solving Level 3/01 _ 3x3 Ranom Matrix/01 _ 3x3 Ranom Matrix/01 _ 3x3 Ranom Matrix.cpp
+++ 07 - Algorithms & Problem Solving Level 3/01 _ 3x3 Ranom Matrix/01 _ 3x3 Ranom Matrix/01 _ 3x3 Ranom Matrix.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -6,16 +7,16 @@ int GenerateRandomNumbers(int from, int to) {
 	return rand() % (to - from + 1) + from;
 }
 
-void Fill2DArrayWithRandomNumbers(int arr[3][3]) {
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
+void Fill2DArrayWithRandomNumbers(int arr[3][3], int rows, int columns) {
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
 			arr[i][j] = GenerateRandomNumbers(1, 100);
 }
 
-void Print2DArrayResult(int arr[3][3]) {
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++)
-			cout << arr[i][j] << " ";
+void Print2DArrayResult(int arr[3][3], int rows, int columns) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++)
+			cout << setw(3) << arr[i][j] << " ";
 		cout << endl;
 	}
 }
@@ -25,9 +26,11 @@ int main()
 	srand((unsigned)time(NULL));
 
 	int arr[3][3];
-	Fill2DArrayWithRandomNumbers(arr);
+	Fill2DArrayWithRandomNumbers(arr, 3, 3);
 
-	Print2DArrayResult(arr);
+	Print2DArrayResult(arr, 3, 3);
+
+	system("pause>0");
 
 	return 0;
 }
