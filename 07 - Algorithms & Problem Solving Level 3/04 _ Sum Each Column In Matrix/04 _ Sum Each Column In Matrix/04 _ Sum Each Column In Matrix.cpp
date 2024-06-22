de1af5a9756873661f7ca171f9c2e8563ef7a4df
MohamadKhaled <mohamadknsult2@gmail.com>
2024-06-22 13:26:14 +0300

    June-2024

---
+++ 07 - Algorithms & Problem Solving Level 3/04 _ Sum Each Column In Matrix/04 _ Sum Each Column In Matrix/04 _ Sum Each Column In Matrix.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int GenerateRandomNumber(int from, int to) {
+	return rand() % (to - from + 1) + from;
+}
+
+void FillMatrixWithRandomNumber(int arr[3][3], int rows, int cols) {
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			arr[i][j] = GenerateRandomNumber(1, 100);
+}
+
+void PrintMatrix(int arr[3][3], int rows, int cols) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}
+}
+
+int ColumnsSum(int arr[3][3], int rows, int cols) {
+	int sum = 0;
+	for (int i = 0; i < rows; i++)
+		sum += arr[i][cols];
+	return sum;
+}
+
+void PrintEachColumnsSum(int arr[3][3], int rows, int cols) {
+	for (int i = 0; i < cols; i++)
+		cout << "column " << i + 1 << " = " << ColumnsSum(arr, rows, i) << endl;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[3][3];
+
+	FillMatrixWithRandomNumber(arr, 3, 3);
+
+	PrintMatrix(arr, 3, 3);
+
+	PrintEachColumnsSum(arr, 3, 3);
+}
