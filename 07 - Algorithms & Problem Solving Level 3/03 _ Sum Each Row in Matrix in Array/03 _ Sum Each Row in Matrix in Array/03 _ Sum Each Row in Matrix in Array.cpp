664ad3e0d811c303400d7f69a965a3cde31cb2ef
MohamadKhaled <mohamadknsult2@gmail.com>
2024-06-22 08:40:29 +0300

    June-2024

--- 07 - Algorithms & Problem Solving Level 3/03 _ Sum Each Row in Matrix in Array/03 _ Sum Each Row in Matrix in Array/03 _ Sum Each Row in Matrix in Array.cpp
+++ 07 - Algorithms & Problem Solving Level 3/03 _ Sum Each Row in Matrix in Array/03 _ Sum Each Row in Matrix in Array/03 _ Sum Each Row in Matrix in Array.cpp
@@ -2,57 +2,54 @@
 
 using namespace std;
 
-short GenerateRandomNumber(int from, int to) {
-	return rand() % (to - from + 1) + from;
+int GenerateRandomNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
 }
 
-void Fill2DArrayWithRandomNumbers(short arr[3][3], short rows, short cols) {
+void FillMatrixWithRandomNumbers(int arr[3][3], short rows, short cols) {
 	for (short i = 0; i < rows; i++)
-	{
 		for (short j = 0; j < cols; j++)
-		{
 			arr[i][j] = GenerateRandomNumber(1, 100);
-		}
-	}
 }
 
-void PrintMatrix(short arr[3][3], short rows, short cols) {
+void PrintMatrix(int arr[3][3], short rows, short cols) {
 	for (short i = 0; i < rows; i++)
 	{
 		for (short j = 0; j < cols; j++)
-		{
 			cout << arr[i][j] << " ";
-		}
 		cout << endl;
 	}
 }
 
-void SumMatrix(short arr[3][3], short rows, short cols) {
-	short sum = 0;
+int RowSum(int arr[3][3], short rowNumber, short cols) {
+	int sum = 0;
+	for (short j = 0; j < cols; j++)
+		sum += arr[rowNumber][j];
+	return sum;
+}
+
+void SumMatrixRowsInArray(int arr[3][3], int arrSum[3], short rows, short cols) {
 	for (short i = 0; i < rows; i++)
-	{
-		for (short j = 0; j < cols; j++)
-		{
-			sum += arr[i][j];
-			if (j == 2) {
-				printf("Row %d Sum = %d\n",i + 1, sum);
-				sum = 0;
-			}
-		}
-	}
+		arrSum[i] = RowSum(arr, i, cols);
+}
+
+void PrintRowsSumArray(int arr[3], short rows) {
+	for (short i = 0; i < rows; i++)
+		printf("Row %d Sum = %d\n", i + 1, arr[i]);
 }
 
 int main()
 {
 	srand((unsigned)time(NULL));
-	
-	short arr[3][3];
 
-	Fill2DArrayWithRandomNumbers(arr, 3, 3);
+	int arr[3][3];
+	int arrSum[3];
+
+	FillMatrixWithRandomNumbers(arr, 3, 3);
 
 	PrintMatrix(arr, 3, 3);
 
-	SumMatrix(arr, 3, 3);
+	SumMatrixRowsInArray(arr, arrSum, 3, 3);
 
-	return 0;
+	PrintRowsSumArray(arrSum, 3);
 }
