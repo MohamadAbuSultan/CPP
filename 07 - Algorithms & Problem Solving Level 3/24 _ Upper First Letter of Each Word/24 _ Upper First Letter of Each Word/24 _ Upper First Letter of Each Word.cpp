e8a4ff73a5b4c0280fd2a55c21411fe30f00cf2c
MohamadKhaled <mohamadknsult2@gmail.com>
2024-08-10 07:06:31 +0300

    August-2024

--- 07 - Algorithms & Problem Solving Level 3/24 _ Upper First Letter of Each Word/24 _ Upper First Letter of Each Word/24 _ Upper First Letter of Each Word.cpp
+++ 07 - Algorithms & Problem Solving Level 3/24 _ Upper First Letter of Each Word/24 _ Upper First Letter of Each Word/24 _ Upper First Letter of Each Word.cpp
@@ -3,26 +3,39 @@
 
 using namespace std;
 
-string InputWord() {
+string ReadWord() {
 	string Word;
 	cout << "Enter a word: ";
 	getline(cin, Word);
 	return Word;
 }
 
-void PrintUpperFirstLetter(string Word) {
+string UpperFirstLetterOfEachWord(string Word) {
 	bool isFirstLetter = true;
 	for (int i = 0; i < Word.length(); i++)
 	{
 		if (Word[i] != ' ' && isFirstLetter)
-			cout  << char(toupper(Word[i]));
-		else if (i != Word.length())
-			cout << Word[i];
+			Word[i] = toupper(Word[i]);
 		isFirstLetter = (Word[i] == ' ' ? true : false);
 	}
+	return Word;
 }
+
+//void PrintUpperFirstLetter(string Word) {
+//	bool isFirstLetter = true;
+//	for (int i = 0; i < Word.length(); i++)
+//	{
+//		if (Word[i] != ' ' && isFirstLetter)
+//			cout  << char(toupper(Word[i]));
+//		else if (i != Word.length())
+//			cout << Word[i];
+//		isFirstLetter = (Word[i] == ' ' ? true : false);
+//	}
+//}
 
 int main()
 {
-	PrintUpperFirstLetter(InputWord());
+	// PrintUpperFirstLetter(ReadWord());
+	string upperLetter = UpperFirstLetterOfEachWord(ReadWord());
+	cout << upperLetter << endl;
 }
