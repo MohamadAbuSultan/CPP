5ca2385dd18259687fc30ecd57d52df59e019796
MohamadKhaled <mohamadknsult2@gmail.com>
2024-08-18 10:21:42 +0300

    August-2024

--- 07 - Algorithms & Problem Solving Level 3/25 _ Lower First Letter of Each Word/25 _ Lower First Letter of Each Word/25 _ Lower First Letter of Each Word.cpp
+++ 07 - Algorithms & Problem Solving Level 3/25 _ Lower First Letter of Each Word/25 _ Lower First Letter of Each Word/25 _ Lower First Letter of Each Word.cpp
@@ -5,24 +5,29 @@ using namespace std;
  
 string ReadWord() {
 	string word;
+
 	cout << "Enter a word: ";
 	getline(cin, word);
+
 	return word;
 }
 
-string LowerFirstWord(string letter) {
+string LowerFirstLetterOfEachWord(string letter) {
 	bool isFirstLetter = true;
+
 	for (short i = 0; i < letter.length(); i++)
 	{
 		if (letter[i] != ' ' && isFirstLetter)
 			letter[i] = tolower(letter[i]);
+
 		isFirstLetter = (letter[i] == ' ' ? true : false);
 	}
+
 	return letter;
 }
 
 int main()
 {
-	string Letter = LowerFirstWord(ReadWord());
+	string Letter = LowerFirstLetterOfEachWord(ReadWord());
 	cout << Letter;
 }
