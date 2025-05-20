2528b3bf7b061b8a7d83f9b27caa16efe10c7e71
MohamadKhaled <mohamadknsult2@gmail.com>
2025-05-20 10:40:02 +0300

    May-2025

---
+++ 07 - Algorithms & Problem Solving Level 3/47 _ Add Clients to File/47 _ Add Clients to File/47 _ Add Clients to File.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+const string ClientsFileName = "Clients.txt";
+
+struct sClient
+{
+	string AccountNumber;
+	string PinCode;
+	string Name;
+	string Phone;
+	double AccountBalance;
+};
+
+sClient ReadNewClient()
+{
+	sClient Client;
+
+	cout << "Enter Account Number? ";
+	// Usage of std::ws will extract allthe whitespace character
+	getline(cin >> ws, Client.AccountNumber);
+	// (ws) removes extra spaces and newlines before reading input. 😊
+
+	cout << "Enter PinCode? ";
+	getline(cin, Client.PinCode);
+
+	cout << "Enter Name? ";
+	getline(cin, Client.Name);
+
+	cout << "Enter Phone? ";
+	getline(cin, Client.Phone);
+
+	cout << "Enter AccountBalance? ";
+	cin >> Client.AccountBalance;
+
+	return Client;
+}
+
+string ConvertRecordToLine(sClient Client, string Seperator = "#//#")
+{
+	string stClientRecord = "";
+
+	stClientRecord += Client.AccountNumber + Seperator;
+	stClientRecord += Client.PinCode + Seperator;
+	stClientRecord += Client.Name + Seperator;
+	stClientRecord += Client.Phone + Seperator;
+	stClientRecord += to_string(Client.AccountBalance);
+
+	return stClientRecord;
+}
+
+void AddDataLineToFile(string FileName, string stDataLine)
+{
+	fstream MyFile;
+
+	MyFile.open(FileName, ios::out | ios::app);
+	// (ios::app) makes sure new data is added to the end of the file instead of overwriting it. 😊
+	// (ios::app) adds new data without deleting old data. 😊
+
+	if (MyFile.is_open())
+	{
+		MyFile << stDataLine << endl;
+		MyFile.close();
+	}
+}
+
+void AddNewClient()
+{
+	sClient Client;
+	Client = ReadNewClient();
+	AddDataLineToFile(ClientsFileName,
+		ConvertRecordToLine(Client));
+}
+
+void AddClients()
+{
+	char AddMore = 'Y';
+
+	do
+	{
+		system("cls");
+		cout << "Adding New Client:\n\n";
+
+		AddNewClient();
+
+		cout << "\nClient Added Successfully, do you want to add more clients ? Y / N ? "; 
+		cin >> AddMore;
+	} while (toupper(AddMore) == 'Y');
+}
+
+int main()
+{
+	AddClients();
+
+	system("pause>0");
+
+	return 0;
+}
---
+++ 07 - Algorithms & Problem Solving Level 3/47 _ Add Clients to File/47 _ Add Clients to File/my_code.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+struct ClientData {
+    string accountNumber;
+    string pinCode;
+    string name;
+    string phone;
+    double accountBalance;
+};
+
+vector<string> SplitString(string input, string delimiter) {
+    vector<string> result;
+    size_t pos = 0;
+    string sWord;
+
+    while ((pos = input.find(delimiter)) != string::npos) {
+        sWord = input.substr(0, pos);
+        result.push_back(sWord);
+        input.erase(0, pos + delimiter.length());
+    }
+    result.push_back(input);
+
+    return result;
+}
+
+ClientData ParseClientDataFromLine(string line, string delimiter) {
+    vector<string> fields = SplitString(line, delimiter);
+    ClientData client;
+
+    if (fields.size() == 5) {
+        client.accountNumber = fields[0];
+        client.pinCode = fields[1];
+        client.name = fields[2];
+        client.phone = fields[3];
+        client.accountBalance = stod(fields[4]);
+    }
+
+    return client;
+}
+
+void PrintClientData(const ClientData& client) {
+    cout << "\nExtracted Client Data:\n";
+    cout << "Account Number: " << client.accountNumber << endl;
+    cout << "PinCode: " << client.pinCode << endl;
+    cout << "Name: " << client.name << endl;
+    cout << "Phone: " << client.phone << endl;
+    cout << "Account Balance: " << client.accountBalance << endl;
+}
+
+void saveAccountToFile(const ClientData& clinetData, const string& filename) {
+    ofstream outFile(filename);
+
+    if (outFile.is_open()) {
+        outFile << "Account Number: " << clinetData.accountNumber << endl;
+        outFile << "Pin Code: " << clinetData.pinCode << endl;
+        outFile << "Name: " << clinetData.name << endl;
+        outFile << "Phone: " << clinetData.phone << endl;
+        outFile << "Account Balance: " << clinetData.accountBalance << endl;
+        outFile.close();
+        cout << "Account details saved successfully!\n";
+    }
+    else {
+        cout << "Error opening file!\n";
+    }
+}
+
+int main() {
+    string line = "A150#//#1234#//#Mohammed Abu-Hadhoud#//#079939999#//#5000.000000";
+
+    cout << "Line Record: " << line << endl;
+
+    ClientData client = ParseClientDataFromLine(line, "#//#");
+
+    PrintClientData(client);
+
+    saveAccountToFile(client, "account.txt");
+    // You can find the file in the same folder as your program. 😊
+    return 0;
+}
